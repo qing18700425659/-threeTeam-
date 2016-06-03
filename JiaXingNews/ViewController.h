af23bf03a448c861a880b0f5cff206e5d5ca2538
DC20160426 <dc@zhaiwen.lan>
2016-06-03 19:49:26 +0800

    Initial Commit

---
+++ JiaXingNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiaXingNews
+//
+//  Created by DC20160426 on 16/6/3.
+//  Copyright © 2016年 DC20160426. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JiaXingNews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JiaXingNews
+//
+//  Created by DC20160426 on 16/6/3.
+//  Copyright © 2016年 DC20160426. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
